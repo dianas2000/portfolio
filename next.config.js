ad7570c5824e0c84301f5eac51be569a75529f37
Diana Stirbu <dianamaria.stirbu@hs-augsburg.de>
2023-11-29 15:32:17 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
     experimental: {
         typedRoutes: true,
-    },
+    }
+
+    output: "export",
+    basePath: "/nextjs-github-pages",
+
+    images: {
+        unoptimized: true,
+      },
 };
 
 module.exports = nextConfig
