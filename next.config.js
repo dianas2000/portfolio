86e90392accac8a9eedc68bf26fe8cd5fe851b8b
Diana Stirbu <dianamaria.stirbu@hs-augsburg.de>
2023-11-29 16:16:18 +0100

    resolving problem

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
     experimental: {
         typedRoutes: true,
     }
-
-    output: "export",
-    basePath: "/nextjs-github-pages",
-
-    images: {
-        unoptimized: true,
-      },
 };
 
 module.exports = nextConfig
